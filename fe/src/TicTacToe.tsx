62c85f92816d5d74263bbde66e4042943e868c84
kevfort <193007420+kevfort@users.noreply.github.com>
2025-01-04 19:48:59 -0500

    fully implemented tic-tac-toe

--- fe/src/TicTacToe.tsx
+++ fe/src/TicTacToe.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 
+
 type Coordinate = [number,number]
 type clickHandler = (index: Coordinate) => void;
 interface buttonProps{
@@ -7,19 +8,19 @@ interface buttonProps{
     cellIndex: Coordinate;
     onClick: (clickHandler);
 }
-function checkWinner({gameBoard}:{gameBoard:string[][]}):boolean{
+function isWinner(gameBoard:string[][]):boolean{
     for(let i = 0; i < gameBoard.length; i++){
-        if((gameBoard[i][0] != "")&&((gameBoard[i][0] === gameBoard[i][1])&&(gameBoard[i][1] === gameBoard[i][2]))){
+        if((gameBoard[i][0] != "")&&((gameBoard[i][0] === gameBoard[i][1])&&(gameBoard[i][0] === gameBoard[i][2]))){
             return true
         }
-        if((gameBoard[i][0] != "")&&((gameBoard[0][i] === gameBoard[1][i])&&(gameBoard[1][i] === gameBoard[2][i]))){
+        if((gameBoard[0][i] != "")&&((gameBoard[0][i] === gameBoard[1][i])&&(gameBoard[0][i] === gameBoard[2][i]))){
             return true
         }
     }
-    if((gameBoard[0][0] != "")&&((gameBoard[0][0] === gameBoard[1][1])&&(gameBoard[1][1] === gameBoard[2][2]))){
+    if((gameBoard[0][0] != "")&&((gameBoard[0][0] === gameBoard[1][1])&&(gameBoard[0][0] === gameBoard[2][2]))){
         return true
     }
-    else if((gameBoard[0][2] != "")&&((gameBoard[0][2] === gameBoard[1][1])&&(gameBoard[1][1] === gameBoard[2][0]))){
+    else if((gameBoard[0][2] != "")&&((gameBoard[0][2] === gameBoard[1][1])&&(gameBoard[0][2] === gameBoard[2][0]))){
         return true
     }
     return false
@@ -27,17 +28,33 @@ function checkWinner({gameBoard}:{gameBoard:string[][]}):boolean{
 function TicTacToe() {    
     const [gameBoard, setGameBoard] = useState<string[][]>([["","",""],["","",""],["","",""]])
     const [currentPlayer, setCurrentPlayer] = useState<string>("X")
+    const [gameOver, setGameOver] = useState<boolean>(false)
     function handleClick(cellIndex: Coordinate):void{
         if(gameBoard[cellIndex[0]][cellIndex[1]] == ""){
             gameBoard[cellIndex[0]][cellIndex[1]] = currentPlayer
-            //setGameBoard(gameBoard)            
-            if(currentPlayer == "X") {
-                setCurrentPlayer((currentPlayer) => currentPlayer = "O")
+            setGameBoard(gameBoard)            
+            if(isWinner(gameBoard)){
+                setGameOver(true)
             }
-            else {
-                setCurrentPlayer((currentPlayer) => currentPlayer = "X")
+            else{
+                if(currentPlayer == "X") {
+                    setCurrentPlayer("O")
+                }
+                else {
+                    setCurrentPlayer("X")
+                }
             }
         }
+    }
+    if(gameOver){
+        return(
+            <>
+                <h1>Game Over, {currentPlayer} wins!</h1>
+                <button onClick={() => window.location.reload()}>
+                    <h2>Reset?</h2>
+                </button>
+            </>
+        )
     }
     return(
         <div className="flex-container">
@@ -50,6 +67,9 @@ function TicTacToe() {
             <Square board={gameBoard} cellIndex={[2,0]} onClick={handleClick}/>
             <Square board={gameBoard} cellIndex={[2,1]} onClick={handleClick}/>
             <Square board={gameBoard} cellIndex={[2,2]} onClick={handleClick}/>   
+            <button onClick={() => window.location.reload()}>
+                <small>Tie? Click here to reset</small>
+            </button>
         </div>
     )
 }
