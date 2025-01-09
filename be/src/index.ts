408e7f46610560222c345beaede0d396b3a20a62
kevfort <193007420+kevfort@users.noreply.github.com>
2025-01-08 19:24:29 -0500

    added user/session Id generation

--- be/src/index.ts
+++ be/src/index.ts
@@ -4,16 +4,32 @@ import express from 'express'
 const app = express();
 const port = 5177;
 
+const roomMap = new Map<string, string>();  //<roomID, room>
+const playerIds: string[] = [];
 type boardArray = [
     [string,string,string],
     [string,string,string],
     [string,string,string]
 ];
 
-/*class Room {
-    private hostID: number;
-    private guestId: number;
-};*/
+class Room {
+    private hostID: string;
+    private guestID: string;
+    private roomID: string;
+    
+    public constructor({hostID, roomID}:{hostID: string, roomID: string}) {
+        this.hostID = hostID;
+        this.guestID = "nullUser";
+        this.roomID = roomID;
+    }
+    public getHost():string{
+        return(this.hostID);
+    }
+    public getGuest():string{
+        return(this.guestID);
+    }
+    
+};
 class Board {
     private boardLayout: boardArray; 
     public constructor() {
@@ -22,10 +38,10 @@ class Board {
     }
 };
 
-let roomMap = new Map<string, string>();  //<roomName, roomID>
+
 
 const base36Chars: string = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-function createRoom(roomName: string){
+function createRoom(roomName: string, hostName: string){
     roomMap.set(roomName, generateBase32ID(6));
     console.log(`Room ${roomMap.get(roomName)} created`);
 }
@@ -41,16 +57,31 @@ function generateBase32ID(length: number):string{
 function display(message: string): string{
     return(`Your message: ${message}`);
 }
+//base path
 app.get('/', (req, res) => {
     res.send('Hello World')
 });
 
+//creates room
 app.get('/create-room/:roomName/:hostName', (req,res) => {
-    createRoom(req.params.roomName);
+    createRoom(req.params.roomName, req.params.hostName);
     res.send(`Room ${req.params.roomName} created by ${req.params.hostName} with identifier ${roomMap.get(req.params.roomName)}`)
 });
 
+app.get('/new-user/:currentID?', (req,res) => {
+    if(req.params.currentID){
+        res.send(`${req.params.currentID}`);
+    }
+    else{
+        let newID: string = generateBase32ID(10);
+        playerIds.push(newID);
+        res.send(`${newID}`);
+    }
+})
 
+app.get('/close-session/:currentID', (req,res) => {
+    playerIds.splice(playerIds.indexOf(req.params.currentID),1);
+})
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
 });
